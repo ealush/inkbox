975aeaf0f12b46dcaee79fafbe9943c92507ac6f
Evyatar <evyatar.a@fiverr.com>
2018-11-29 01:44:13 +0200

    Updates

---
+++ precache-manifest.93503eb3630bd5ec8901d670f6e675e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c4968b08a89a1ada79d6",
+    "url": "/inkbox/static/js/runtime~main.c4968b08.js"
+  },
+  {
+    "revision": "01a2c66734bbc7bd832d",
+    "url": "/inkbox/static/js/main.01a2c667.chunk.js"
+  },
+  {
+    "revision": "1ce17669b036163428ee",
+    "url": "/inkbox/static/js/1.1ce17669.chunk.js"
+  },
+  {
+    "revision": "01a2c66734bbc7bd832d",
+    "url": "/inkbox/static/css/main.0d73a090.chunk.css"
+  },
+  {
+    "revision": "77bdf346d28d6fdb8d74abf286fae1a4",
+    "url": "/inkbox/index.html"
+  }
+];
