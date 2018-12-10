9948330c5a8c0e774ba652bc90427eb6e358f223
Evyatar <evyatar.a@fiverr.com>
2018-12-10 22:46:15 +0200

    Updates

---
+++ precache-manifest.a06b77281975c7ac4e2e0adf16bbaa41.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c4968b08a89a1ada79d6",
+    "url": "/inkbox/static/js/runtime~main.c4968b08.js"
+  },
+  {
+    "revision": "76bc8e333d7296173384",
+    "url": "/inkbox/static/js/main.76bc8e33.chunk.js"
+  },
+  {
+    "revision": "7f379551198606991ac6",
+    "url": "/inkbox/static/js/1.7f379551.chunk.js"
+  },
+  {
+    "revision": "76bc8e333d7296173384",
+    "url": "/inkbox/static/css/main.b2ef242a.chunk.css"
+  },
+  {
+    "revision": "8a0ca44e883ebd2c4f3eb1b0306a6e88",
+    "url": "/inkbox/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/inkbox/precache-manifest.e6f415248a7272237bdd95821493606f.js"
+  "/inkbox/precache-manifest.a06b77281975c7ac4e2e0adf16bbaa41.js"
 );
 
 workbox.clientsClaim();
